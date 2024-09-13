12a8495317ae17a053cabbe6235334165f7c8eff
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-13 15:51:52 +0200

    bonus aggiunta AppHero con jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default{
   components:{
-    AppTitle
+    AppTitle,
+    AppHero
   },
     data() {
         return {
@@ -14,7 +16,10 @@ export default{
 </script>
 
 <template>
+
    <AppTitle/>
+   <AppHero/>
+
 </template>
 
 <style scoped>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+<div class="jumbotron">
+    <div class="content">Jumbotron</div>
+</div>
+</template>
+
+<style scoped>
+.jumbotron{
+    height: 300px;
+    background-color: plum;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 35px;
+    font-weight: 700;
+}
+</style>
